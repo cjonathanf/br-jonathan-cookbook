dc9f088771d1f02d59b0836f89a0d3a7f083578b
cjonathanf <cferreirajonathan@gmail.com>
2020-05-10 23:07:01 -0300

    Finalização das estruturas de repetição

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,6 @@
 
-puts "Bem - Vindo ao Cookbook, sua rede social de receitas"
-
+puts "========== Bem - Vindo ao Cookbook, sua rede social de receitas =========="
+puts
 
 receitas = []
 
@@ -14,31 +14,44 @@ opcao = gets.to_i()
 
 while(opcao != 3) do
     
-    puts "Digite o nome da receita"
-    nome = gets.chomp() 
-
-    receitas << nome
-
-    puts
-    puts "Receita #{nome} cadastrada com sucesso!"
-    puts
-
-    puts "=========== RECEITAS CADASTRADAS ==========="
-    # for receita in receitas do
-        
-    #     puts receita
-    #     puts
-
-    # end
-
-    receitas.each do |receita|
-        puts receita
+    if(opcao == 1)
+        puts "Digite o nome da receita"
+        nome = gets.chomp() 
+    
+        receitas << nome
+    
         puts
-    end
+        puts "Receita #{nome} cadastrada com sucesso!"
+        puts
+    
+    elsif(opcao == 2)
+        puts "=========== RECEITAS CADASTRADAS ==========="
+        # for receita in receitas do
+            
+        #     puts receita
+        #     puts
 
+        # end
+
+        receitas.each do |receita|
+            puts receita
+            puts
+
+        end 
+    else 
+        puts "Opção Inválida"
+    
+    end 
+
+    puts "[1] Para cadastrar uma nova receita"
+    puts "[2] Para visualizar uma receita cadastrada"
+    puts "[3] Para sair"
+
+    print "Escolha uma das opções acima: "
+    opcao = gets.to_i()
 
 end
 
 puts
-puts "Obrigado por utilizar o cookbook"
+puts "Obrigado por utilizar o cookbook!"
 puts
